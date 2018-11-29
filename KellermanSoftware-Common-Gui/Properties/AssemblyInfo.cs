61a2f7cefa05d1fb31ebb2dde4b3e0226c350d33
Greg Finzer <gfinzer@hotmail.com>
2018-11-29 12:55:35 -0500

    Change to multi-target

--- KellermanSoftware-Common-Gui/Properties/AssemblyInfo.cs
+++ KellermanSoftware-Common-Gui/Properties/AssemblyInfo.cs
@@ -1,15 +1,11 @@
+using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GuiErrorHandler")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kellerman Software")]
-[assembly: AssemblyProduct("GuiErrorHandler")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,14 +17,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d7b9c607-59b9-44a9-bb29-cca2280aa693")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
+
+//Medium Trust
+[assembly: AllowPartiallyTrustedCallers]
