8091ad475806e214bc4b96e91bfc5d40ea7a9a53
Greg Finzer <gfinzer@hotmail.com>
2018-11-30 15:31:17 -0500

    Microsoft Project SDK cannot do winform nuget builds correctly with resources.  Reverting.

--- KellermanSoftware-Common-Gui/Properties/AssemblyInfo.cs
+++ KellermanSoftware-Common-Gui/Properties/AssemblyInfo.cs
@@ -1,11 +1,15 @@
-using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("Kellerman-Common-Gui")]
+[assembly: AssemblyDescription("Common forms and utility methods for WinForms including an About Box, Error Dialog, Nag Dialog, and Registration Dialog.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kellerman Software")]
+[assembly: AssemblyProduct("Kellerman-Common-Gui")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +21,14 @@ using System.Security;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d7b9c607-59b9-44a9-bb29-cca2280aa693")]
 
-[assembly: CLSCompliant(true)]
-
-//Medium Trust
-[assembly: AllowPartiallyTrustedCallers]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.12.0.0")]
+[assembly: AssemblyFileVersion("1.12.0.0")]
