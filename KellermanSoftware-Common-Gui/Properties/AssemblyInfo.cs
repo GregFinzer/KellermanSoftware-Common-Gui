4af247378ca1e8e26c01c07b3ede09a9eea53c70
Greg Finzer <gfinzer@hotmail.com>
2022-01-06 14:46:49 -0500

    Copyright changed to 2022

--- KellermanSoftware-Common-Gui/Properties/AssemblyInfo.cs
+++ KellermanSoftware-Common-Gui/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kellerman Software")]
 [assembly: AssemblyProduct("Kellerman-Common-Gui")]
-[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
